dde0d68f379a150a5a27309d7b35d96592a1053d
maaikevp <146950822+maaikevp@users.noreply.github.com>
2024-03-19 08:47:43 +0100

    Additional adjustments

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -30893,7 +30893,7 @@ $RefreshReg$(_c, "LoginView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","react-bootstrap/Button":"aPzUt","react-bootstrap/Form":"iBZ80","@parcel/transformer-js/src/esmodule-helpers.js":"1CSS1","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gASnm"}],"4OGiN":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"1CSS1","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gASnm","react-bootstrap/Button":"aPzUt","react-bootstrap/Form":"iBZ80"}],"4OGiN":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$73d1 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
@@ -31072,6 +31072,6 @@ $RefreshReg$(_c, "SignupView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","react-bootstrap/Button":"aPzUt","react-bootstrap/Form":"iBZ80","@parcel/transformer-js/src/esmodule-helpers.js":"1CSS1","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gASnm"}]},["3np7Y","1zrH5","d8Dch"], "d8Dch", "parcelRequireaec4")
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"1CSS1","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gASnm","react-bootstrap/Button":"aPzUt","react-bootstrap/Form":"iBZ80"}]},["3np7Y","1zrH5","d8Dch"], "d8Dch", "parcelRequireaec4")
 
 //# sourceMappingURL=index.b4b6dfad.js.map
